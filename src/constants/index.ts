42be1fc751f4542af7fb43e13866702c4caa631d
Don Gabriel Ablay <dgabrielablay@gmail.com>
2026-02-16 15:39:47 +0800

    feature: add src/constants

---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+export const TOTAL_CYCLES = 52;
+export const API_URL = 'http://localhost:3001/api';
+export const CURRENCY_SYMBOL = '₱';
+
+// These are now defaults used only for initial state or fallbacks
+export const DEFAULT_ANCHOR_DATE = '2026-02-06';
+export const DEFAULT_FIXED_INTERVAL = 14;
